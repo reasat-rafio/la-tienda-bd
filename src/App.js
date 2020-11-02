e6ff608bf6db4f1b9521c864a7820b32afb01dc0
reasat-rafio <rafioar96@gmail.com>
2020-11-02 15:50:17 +0600

    basic css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-   return <div></div>;
+   return (
+      <div className="grid-container">
+         <header>
+            <a href="/">La tienda BD</a>
+         </header>
+         <main>Product List</main>
+         <footer>All right is reserved.</footer>
+      </div>
+   );
 }
 
 export default App;
