6966fb6ae267b3e3decc915736f9d5c8c936c56f
reasat-rafio <rafioar96@gmail.com>
2020-11-03 19:01:09 +0600

    asd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+//Feature-1
 import "./App.css";
+import React, { Component, useState } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
+const App = () => {
+   const [state, setState] = useState({
+      products: data.products,
+      size: "",
+      sort: "",
+   });
+
+   console.log(state);
+
    return (
       <div className="grid-container">
          <header>
             <a href="/">La tienda BD</a>
          </header>
-         <main>Product List</main>
+         <main>
+            <div className="content">
+               <div className="main">
+                  <Products products={state.products} />
+               </div>
+               <div className="sidebar">Cart items</div>
+            </div>
+         </main>
          <footer>All right is reserved.</footer>
       </div>
    );
-}
+};
 
 export default App;
